01d0c367470757c00d22bf123f2258ed7ef215d5
TheVoid <593767573@qq.com>
2020-01-29 21:53:53 +0800

    加入了对WebSocket的支持

--- src/main/java/cn/teamthevoid/AiTankArenaServer/Main.java
+++ src/main/java/cn/teamthevoid/AiTankArenaServer/Main.java
@@ -2,6 +2,8 @@ package cn.teamthevoid.AiTankArenaServer;
 
 import cn.teamthevoid.AiTankArenaServer.handler.JoinGameHandler;
 import cn.teamthevoid.AiTankArenaServer.handler.ServerInfoHandler;
+import cn.teamthevoid.AiTankArenaServer.handler.websocket.ProtoBufToWebSocketBinaryDataEncoder;
+import cn.teamthevoid.AiTankArenaServer.handler.websocket.WebSocketBinaryDataDecoder;
 import cn.teamthevoid.AiTankArenaServer.message.operation.Operation;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
@@ -11,18 +13,25 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
 import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+import io.netty.handler.stream.ChunkedWriteHandler;
 
 public class Main {
     public void run() throws Exception {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(); // (1)
-        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        EventLoopGroup socketBossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup socketWorkerGroup = new NioEventLoopGroup();
+        EventLoopGroup websocketBossGroup = new NioEventLoopGroup(); // (1)
+        EventLoopGroup websocketWorkerGroup = new NioEventLoopGroup();
         try {
-            ServerBootstrap b = new ServerBootstrap(); // (2)
-            b.group(bossGroup, workerGroup)
+            ServerBootstrap socketBootstrap = new ServerBootstrap(); // (2)
+            socketBootstrap.group(socketBossGroup, socketWorkerGroup)
                     .channel(NioServerSocketChannel.class) // (3)
                     .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
                         @Override
@@ -42,20 +51,44 @@ public class Main {
                     .option(ChannelOption.SO_BACKLOG, 128)          // (5)
                     .childOption(ChannelOption.TCP_NODELAY, true);
 
-            ChannelFuture f = b.bind(8080).sync(); // (7)
-            f.channel().closeFuture().sync();
+            ServerBootstrap websocketBootstrap = new ServerBootstrap(); // (2)
+            websocketBootstrap.group(websocketBossGroup, websocketWorkerGroup)
+                    .channel(NioServerSocketChannel.class) // (3)
+                    .childHandler(new ChannelInitializer<SocketChannel>() { // (4)
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            var pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpServerCodec());
+                            pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+                            pipeline.addLast(new ChunkedWriteHandler());
+                            pipeline.addLast(new WebSocketServerCompressionHandler());
+                            pipeline.addLast(new WebSocketServerProtocolHandler("/", null, true));
+                            pipeline.addLast(new WebSocketBinaryDataDecoder());
+                            pipeline.addLast(new ProtobufDecoder(Operation.getDefaultInstance()));
+                            pipeline.addLast(new ProtoBufToWebSocketBinaryDataEncoder());
+                            pipeline.addLast(new ServerInfoHandler());
+                            pipeline.addLast(new JoinGameHandler());
+
+//                            pipeline.addLast(new TestOutHandler() );
+
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)          // (5)
+                    .childOption(ChannelOption.TCP_NODELAY, true);
+
+
+            ChannelFuture socketFuture = socketBootstrap.bind(8080); // (7)
+            ChannelFuture websocketFuture = websocketBootstrap.bind(8081).sync(); // (7)
+
+            socketFuture.channel().closeFuture().sync();
+            websocketFuture.channel().closeFuture().sync();
         } finally {
-            workerGroup.shutdownGracefully();
-            bossGroup.shutdownGracefully();
+            socketWorkerGroup.shutdownGracefully();
+            socketBossGroup.shutdownGracefully();
         }
     }
 
     public static void main(String[] args) throws Exception {
-        int port = 8080;
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        }
-
         new Main().run();
     }
 }
