2c7037b84c9107564b4f4c0f9b2a1b3d8606527c
TheVoid <593767573@qq.com>
2020-02-12 00:25:36 +0800

    修改了游戏准备界面的message

--- src/main/protobuf/protobuf.sh
+++ src/main/protobuf/protobuf.sh
@@ -1,2 +1,3 @@
-protoc -I=./ --java_out=../java/ --python_out=../python/ ./*.proto
-cp ./*.proto ../javascript/protobuf
+#rm -rf ../java/message/
+protoc -I=./ --java_out=../java/ --python_out=../python/ --csharp_out=../unity/Assets/Script/Protobuf ./*.proto
+#cp ./*.proto ../javascript/protobuf
