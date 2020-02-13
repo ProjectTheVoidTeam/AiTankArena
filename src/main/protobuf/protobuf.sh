196bc7726196122adfc4bdb3a2d77ea30b0f1ef6
TheVoid <593767573@qq.com>
2020-02-14 07:17:47 +0800

    更新.proto

--- src/main/protobuf/protobuf.sh
+++ src/main/protobuf/protobuf.sh
@@ -1,3 +1,3 @@
 #rm -rf ../java/message/
-protoc -I=./ --java_out=../java/ --python_out=../python/ --csharp_out=../unity/Assets/Script/Protobuf ./*.proto
+protoc -I=./ --java_out=../java/ --python_betterproto_out=../python/arena_lib --csharp_out=../unity/Assets/Script/Protobuf ./*.proto
 #cp ./*.proto ../javascript/protobuf
